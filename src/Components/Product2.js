303344eae6c22c781a6aaade6fb89cd99beadbec
John Doe <mouryanaru001@gmail.com>
2024-06-12 09:18:39 +0530

    123

--- src/Components/Product2.js
+++ src/Components/Product2.js
@@ -6,10 +6,10 @@ const Product2 = () => {
   const [data, setData] = useState({
     'Price_June07': '₹1,599',
     'Price_June08': '₹1,599',
-      'Price_June09': '₹1,599',
+    'Price_June09': '₹1,599',
     'Price_June10': '₹1,599',
     'Price_June11': '₹1,599',
-    'Price_June11': '₹1,249'
+    'Price_June12': '₹1,249',
 
   });
 
